7f97b99dc748d3e239662b335a82e49852793909
Kendrick Ang <kendrick.wh@outlook.com>
2023-05-28 12:42:09 +0800

    ignite scaffold message createGame black red --module checkers --response gameIndex

--- x/checkers/types/errors.go
+++ x/checkers/types/errors.go
@@ -8,7 +8,7 @@ import (
 
 // x/checkers module sentinel errors
 var (
-	ErrInvalidBlack = sdkerrors.Register(ModuleName, 1100, "invalid black address")
-	ErrInvalidRed = sdkerrors.Register(ModuleName, 1101, "invalid red address")
+	ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1100, "invalid black address")
+	ErrInvalidRed       = sdkerrors.Register(ModuleName, 1101, "invalid red address")
 	ErrGameNotParseable = sdkerrors.Register(ModuleName, 1102, "game not parseable")
 )
